474376938fe67cec10e836212ad52ce388d61531
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-20 23:36:39 +0800

    comment

---
+++ print_characters_at_even_index_in_a_string.py
@@ -0,0 +1,2 @@
+#Exercise 3: Print characters from a string that are present at an even index number
+#Write a program to accept a string from the user and display characters that are present at an even index number.
