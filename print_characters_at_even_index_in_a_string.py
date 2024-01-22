0a6c612ef57d327d9db3f4132d1dec33aa3a1e2a
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 12:34:35 +0800

    string

--- print_characters_at_even_index_in_a_string.py
+++ print_characters_at_even_index_in_a_string.py
@@ -2,9 +2,12 @@
 #Write a program to accept a string from the user and display characters that are present at an even index number.
 
 # Accept a string from the user
+user_input = input("Enter a word:")
+print("Original string is", user_input)
 
 # Display characters at even index numbers
 
 # Print the result
+
 
 
